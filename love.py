0e1c3bed22022c05c4d434fe1afd63375f4e526d
vynnih <celengmegendil@gmail.com>
2026-09-22 12:27:52 +0800

    Update love.py

--- love.py
+++ love.py
@@ -46,7 +46,7 @@ a.goto(-2,75)
 a.pendown()
 a.color("red")
 style = ('courier',10,'italic')
-a.write('SELMA',font=style,align='left')
+a.write('CAPIAA',font=style,align='left')
 a.hideturtle()
 
 a = turtle.Turtle()
@@ -64,7 +64,7 @@ a.goto(-150,-130)
 a.pendown()
 a.color("red")
 style = ('courier',30,'italic')
-a.write('TAPI BOONG..😁',font=style,align='left')
+a.write('I LOVE U 🌹',font=style,align='left')
 a.hideturtle()
 
 
